e718f81483312723a3aa1bec9905a67666544441
Oladayo Ogundipe <oladayogn@gmail.com>
2021-07-01 11:54:14 -0700

    include the env for testing

---
+++ src/components/env.js
@@ -0,0 +1,5 @@
+module.exports = {
+  REACT_APP_MOVIE_DB_API_KEY: "",
+  REACT_APP_API_DOMAIN: "https://api.themoviedb.org/3/movie/upcoming",
+  REACT_APP_API_BASE_IMAGE_URL: "https://image.tmdb.org/t/p/original",
+};
